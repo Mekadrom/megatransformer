many changes to fix huggingface integration, loss calculation, and misc

src/megatransformer/criteria.py
@@ -47,9 +47,8 @@ class LMLoss(nn.Module):
 
     def forward(self, logits: torch.Tensor, labels: torch.Tensor):
         logits = logits.view(-1, logits.size(-1))
-        
         labels = labels.view(-1)
-        
+
         loss = F.cross_entropy(
             logits, 
             labels,

src/megatransformer/infinite_multihead_attn.py
@@ -7,9 +7,9 @@ from . import transformer_utils
 import torch
 import torch.nn.functional as F
 
-class InfiniMultiHeadAttention(nn.Module):
+class InfiniteMultiHeadAttention(nn.Module):
     def __init__(self, device, model_config, attn_config, self_attn, in_decoder=False):
-        super(InfiniMultiHeadAttention, self).__init__()
+        super(InfiniteMultiHeadAttention, self).__init__()
 
         self.device = device
         self.attn_config = attn_config

src/megatransformer/megatransformer.py
@@ -1,7 +1,7 @@
 from torch import nn
 from typing import Optional, Union
 
-from . import embedding_mlp, millions_moe, phi3_mlp, per_lang_embedding, positionwise_fcn, multihead_attn, sum, transformer_utils, criteria, grouped_query_attn, infini_multihead_attn
+from . import embedding_mlp, infinite_multihead_attn, millions_moe, phi3_mlp, per_lang_embedding, positionwise_fcn, multihead_attn, sum, transformer_utils, criteria, grouped_query_attn
 
 import admin_torch
 import math
@@ -15,51 +15,6 @@ class MegaTransformerOutput:
         self.moe_loss = moe_loss
         self.decoder_gating_variances = decoder_gating_variances
         self.encoder_gating_variances = encoder_gating_variances
-
-def init_weights(model: nn.Module,
-                 d_model: int,
-                 init_weights_from: str = 'glorot_uniform',
-                 init_weights_gain: float = 1.0,
-                 tie_embeddings=False):
-    for p in model.parameters():
-        # glorot initialization needs at least two dimensions on the tensor
-        if p.dim() > 1:
-            if init_weights_from in ['glorot_uniform', 'xavier_uniform']:
-                nn.init.xavier_uniform_(p, gain=init_weights_gain)
-            elif init_weights_from in ['glorot_normal', 'xavier_normal']:
-                nn.init.xavier_normal_(p, gain=init_weights_gain)
-            elif init_weights_from == 'kaiming_uniform':
-                nn.init.kaiming_uniform_(p)
-            elif init_weights_from == 'kaiming_normal':
-                nn.init.kaiming_normal_(p)
-            elif init_weights_from == 'orthogonal':
-                nn.init.orthogonal_(p)
-            else:
-                raise Exception(f"Unknown weight initialization method: {init_weights_from}")
-
-    # share weights between the embedding layers and the logit layer
-    if isinstance(model, MegaTransformer):
-        encoder: Encoder = model.encoder
-        decoder: Decoder = model.decoder
-        if isinstance(encoder.embed_tokens, nn.Embedding) and isinstance(decoder.embed_tokens, nn.Embedding):
-            encoder_embedding: nn.Embedding = encoder.embed_tokens
-            decoder_embedding: nn.Embedding = decoder.embed_tokens
-            nn.init.normal_(encoder_embedding.weight, mean=0., std=d_model ** -0.5)
-            if tie_embeddings:
-                decoder_embedding.weight = encoder_embedding.weight
-                decoder.lm_head.weight = decoder_embedding.weight
-        elif isinstance(encoder.embed_tokens, embedding_mlp.EmbeddingMLP) \
-            and isinstance(decoder.embed_tokens, embedding_mlp.EmbeddingMLP) \
-            and isinstance(decoder.lm_head, nn.Sequential):
-            encoder_embedding: embedding_mlp.EmbeddingMLP = encoder.embed_tokens
-            decoder_embedding: embedding_mlp.EmbeddingMLP = decoder.embed_tokens
-
-            nn.init.normal_(encoder_embedding.embedding.weight, mean=0., std=d_model ** -0.5)
-
-            if tie_embeddings:
-                decoder_embedding.embedding.weight = encoder_embedding.embedding.weight
-                decoder.lm_head[-1].weight = decoder.embed_tokens.embedding.weight
-    print("Model initialized.")
 
 class EncoderLayer(nn.Module):
     def __init__(self, device, model_config):
@@ -81,7 +36,7 @@ class EncoderLayer(nn.Module):
         if self_attn_config.attn_impl == 'gqa':
             self.self_attn: nn.Module = grouped_query_attn.GroupedQueryMultiHeadAttention(device, model_config, self_attn_config, self_attn=True, in_decoder=False)
         elif self_attn_config.attn_impl == 'infini':
-            self.self_attn: nn.Module = infini_multihead_attn.InfiniMultiHeadAttention(device, model_config, self_attn_config, self_attn=True, in_decoder=False)
+            self.self_attn: nn.Module = infinite_multihead_attn.InfiniteMultiHeadAttention(device, model_config, self_attn_config, self_attn=True, in_decoder=False)
         else:
             self.self_attn: nn.Module = multihead_attn.MultiHeadAttention(device, model_config, self_attn_config, self_attn=True, in_decoder=False)
 
@@ -283,7 +238,7 @@ class DecoderLayer(nn.Module):
         if self_attn_config.attn_impl == 'gqa':
             self.self_attn: nn.Module = grouped_query_attn.GroupedQueryMultiHeadAttention(device, model_config, self_attn_config, self_attn=True, in_decoder=True)
         elif self_attn_config.attn_impl == 'infini':
-            self.self_attn: nn.Module = infini_multihead_attn.InfiniMultiHeadAttention(device, model_config, self_attn_config, self_attn=True, in_decoder=True)
+            self.self_attn: nn.Module = infinite_multihead_attn.InfiniteMultiHeadAttention(device, model_config, self_attn_config, self_attn=True, in_decoder=True)
         else:
             self.self_attn: nn.Module = multihead_attn.MultiHeadAttention(device, model_config, self_attn_config, self_attn=True, in_decoder=True)
 
@@ -291,7 +246,7 @@ class DecoderLayer(nn.Module):
             if cross_attn_config.attn_impl == 'gqa':
                 self.cross_attn: nn.Module = grouped_query_attn.GroupedQueryMultiHeadAttention(device, model_config, cross_attn_config, self_attn=False, in_decoder=True)
             elif cross_attn_config.attn_impl == 'infini':
-                self.cross_attn: nn.Module = infini_multihead_attn.InfiniMultiHeadAttention(device, model_config, cross_attn_config, self_attn=False, in_decoder=True)
+                self.cross_attn: nn.Module = infinite_multihead_attn.InfiniteMultiHeadAttention(device, model_config, cross_attn_config, self_attn=False, in_decoder=True)
             else:
                 self.cross_attn: nn.Module = multihead_attn.MultiHeadAttention(device, model_config, cross_attn_config, self_attn=False, in_decoder=True)
         else:
@@ -394,8 +349,8 @@ class Decoder(nn.Module):
         if self.positional_encoding_type != 'rotary':
             self.tensor_positional_encoding = nn.Parameter(transformer_utils.get_tensor_positional_encoding(self.device, self.d_model, self.positional_encoding_dim, self.learnable_positional_encoding, self.maxlen))
 
-        self.moe_criteria = criteria.DecoderOnlyMoELoss(decoder_config.moe_diversity_loss_coefficient)
         self.main_criteria = criteria.LMLoss(ignore_index=model_config.padding_value, eps=model_config.label_smoothing)
+        self.moe_criteria = criteria.DecoderOnlyMoELoss(decoder_config.moe_diversity_loss_coefficient)
 
     def make_decoder_layers(self, n_layers, param_sharing_type, m_independent_layers) -> nn.ModuleList:
         def new_decoder_layer():
@@ -556,8 +511,6 @@ class MegaTransformer(nn.Module):
 
         self.main_criteria = criteria.LMLoss(ignore_index=model_config.padding_value, eps=model_config.label_smoothing)
         self.moe_criteria = criteria.TransformerMoELoss(model_config.moe_diversity_loss_coefficient)
-
-        init_weights(self, model_config.d_model, model_config.init_weights_from, model_config.init_weights_gain, model_config.tie_embeddings)
 
     def forward(
             self,

src/megatransformer/multihead_attn.py
@@ -35,7 +35,6 @@ class MultiHeadAttention(nn.Module):
         self.v_bias = attn_config.v_bias
         self.o_bias = attn_config.o_bias
         self.heads_activation_function = attn_config.heads_activation_function
-        self.use_infinite_attention = attn_config.use_infinite_attention
         self.infinite_attention_n_segments = attn_config.infinite_attention_n_segments
         self.infinite_attention_update = attn_config.infinite_attention_update
         self.use_grok_scaled_attn = attn_config.use_grok_scaled_attn
@@ -75,9 +74,7 @@ class MultiHeadAttention(nn.Module):
 
             attention_mask = attention_mask.unsqueeze(1).unsqueeze(1).to(torch.bool)
 
-            # attention_weights = attention_weights.masked_fill_(attention_mask, -float('inf'))
-            attention_weights = attention_weights + attention_mask
-
+            attention_weights = attention_weights.masked_fill_(attention_mask, -float('inf'))
         if self.self_attn:
             attention_weights = attention_weights.masked_fill_(~self.causal_mask[:attention_weights.shape[-2], :attention_weights.shape[-1]], -float('inf'))
 
@@ -135,30 +132,31 @@ class MultiHeadAttention(nn.Module):
             q_heads = self.rotary_embedding.rotate_queries_or_keys(q_heads, seq_dim=-2)
             k_heads = self.rotary_embedding.rotate_queries_or_keys(k_heads.unsqueeze(0), seq_dim=-2).squeeze(0) # adds a singleton dimension for the rotation operation and then removes it for the torch compiler
 
-        if return_attn_values:
-            attention_weights = torch.einsum('...htq,...hTq->...htT', q_heads, k_heads)
+        # if return_attn_values:
+        attention_weights = torch.einsum('...htq,...hTq->...htT', q_heads, k_heads)
 
-            attention_weights = (1.0 / (self.d_queries ** 0.5)) * attention_weights
-            if bool(self.use_grok_scaled_attn):
-                attention_weights = 30.0 * torch.tanh(attention_weights / 30.0) # grok version of scaled attention
+        attention_weights = (1.0 / (self.d_queries ** 0.5)) * attention_weights
+        if bool(self.use_grok_scaled_attn):
+            attention_weights = 30.0 * torch.tanh(attention_weights / 30.0) # grok version of scaled attention
 
-            attention_weights = self.mask_attention(attention_weights, attention_mask)
-            attention_weights = self.attn_softmax(attention_weights)
-            attention_weights_for_visualization = [attention_weights.clone().detach()]
+        attention_weights = self.mask_attention(attention_weights, attention_mask)
+        attention_weights = self.attn_softmax(attention_weights)
+        attention_weights_for_visualization = [attention_weights.clone().detach()]
 
-            attention_weights = self.attn_dropout(attention_weights)
+        attention_weights = self.attn_dropout(attention_weights)
 
-            # Calculate sequences as the weighted sums of values based on these softmax weights
-            sequences = torch.einsum('...htT,...hTv->...htv', attention_weights, v_heads)
-        else:
-            sequences = F.scaled_dot_product_attention(
-                q_heads,
-                k_heads,
-                v_heads,
-                attn_mask=attention_mask,
-                dropout_p=self.dropout,
-                is_causal=self.self_attn
-            )
+        # Calculate sequences as the weighted sums of values based on these softmax weights
+        sequences = torch.einsum('...htT,...hTv->...htv', attention_weights, v_heads)
+        # else:
+        #     attention_weights_for_visualization = []
+        #     sequences = F.scaled_dot_product_attention(
+        #         q_heads,
+        #         k_heads,
+        #         v_heads,
+        #         attn_mask=attention_mask,
+        #         dropout_p=self.dropout,
+        #         is_causal=self.self_attn
+        #     )
 
         sequences = sequences.permute(0, 2, 1, 3)
 

src/megatransformer/phi3_mlp.py
@@ -1,7 +1,8 @@
 from torch import nn
 
-from . import transformer_utils
+from . import sparse_moe, transformer_utils
 
+import copy
 import torch
 
 class Phi3MLP(nn.Module):
@@ -11,18 +12,38 @@ class Phi3MLP(nn.Module):
         ffn_config = model_config.ffn_config
 
         self.d_model = model_config.d_model
+        self.dropout = model_config.dropout
+        self.norm = model_config.norm
+        self.norm_eps = model_config.norm_eps
+
+        self.ffn_type = ffn_config.ffn_type
         self.d_inner = ffn_config.d_inner
         self.activation_function = ffn_config.activation_function
+        self.ffn_bias = ffn_config.ffn_bias
 
-        self.gate_up_proj = nn.Linear(self.d_model, 2 * self.d_inner, bias=False)
-        self.down_proj = nn.Linear(self.d_inner, self.d_model, bias=False)
+        self.layer_norm = self.norm(self.d_model, self.norm_eps)
+        self.activation = transformer_utils.create_activation_function(self.d_inner, self.activation_function)
+        self.dropout = nn.Dropout(self.dropout)
+        
+        self.expand: nn.Module
+        if self.ffn_type == 'sparse':
+            sparse_ffn_config = copy.deepcopy(ffn_config)
+            sparse_ffn_config.d_inner = 2 * self.d_inner
+            self.expand = sparse_moe.SparseMoE(model_config)
+        else:
+            self.expand = nn.Linear(self.d_model, 2 * self.d_inner, bias=self.ffn_bias)
+        self.condense = nn.Linear(self.d_inner, self.d_model, bias=self.ffn_bias)
 
-        self.activation_fn = transformer_utils.create_activation_function(self.d_inner, self.activation_function)
+    def forward(self, sequences: torch.Tensor) -> torch.Tensor:
+        sequences = self.layer_norm(sequences)
 
-    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
-        up_states = self.gate_up_proj(hidden_states)
+        expanded = self.expand(sequences)
 
-        gate, up_states = up_states.chunk(2, dim=-1)
-        up_states = up_states * self.activation_fn(gate)
+        gate, expanded = expanded.chunk(2, dim=-1)
+        expanded = expanded * self.activation_function(gate)
 
-        return self.down_proj(up_states)
+        condensed = self.condense(expanded)
+
+        condensed = self.dropout(condensed)
+
+        return condensed

src/megatransformer/transformer_utils.py
@@ -1,7 +1,9 @@
 from positional_encodings.torch_encodings import PositionalEncoding2D
 from torch import nn
+from torch.utils.tensorboard import SummaryWriter
+from typing import Union
 
-from . import swiglu
+from . import swiglu, megatransformer, multihead_attn, positionwise_fcn, grouped_query_attn, phi3_mlp, millions_moe, infinite_multihead_attn
 
 import math
 import torch
@@ -56,3 +58,172 @@ def get_tensor_positional_encoding(device, d_model: int, positional_encoding_dim
     ).to(device)
     positional_encoding.requires_grad = learnable_positional_encoding
     return positional_encoding
+
+def sanitize_model(model):
+    if hasattr(model, '_orig_mod'):
+        return sanitize_model(model._orig_mod)
+    
+    return model
+
+def init_weights(model: nn.Module,
+                 d_model: int,
+                 init_weights_from: str = 'glorot_uniform',
+                 init_weights_gain: float = 1.0,
+                 tie_embeddings=False):
+    if isinstance(model, megatransformer.MegaTransformer):
+        init_weights(model.encoder, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        init_weights(model.decoder, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+    elif isinstance(model, megatransformer.Encoder):
+        encoder: megatransformer.Encoder = model
+        init_weights(encoder.embed_tokens, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        for encoder_layer in encoder.encoder_layers:
+            init_weights(encoder_layer, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        init_weights(encoder.post_encoder_norm, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+    elif isinstance(model, megatransformer.EncoderLayer):
+        encoder_layer: megatransformer.EncoderLayer = model
+        init_weights(encoder_layer.self_attn, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        init_weights(encoder_layer.ffn, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+    elif isinstance(model, megatransformer.Decoder):
+        decoder: megatransformer.Decoder = model
+        init_weights(decoder.embed_tokens, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        for decoder_layer in decoder.decoder_layers:
+            init_weights(decoder_layer, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        init_weights(decoder.post_decoder_norm, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        if tie_embeddings:
+            decoder.lm_head.weight = decoder.embed_tokens.weight
+        else:
+            init_weights(decoder.lm_head, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+    elif isinstance(model, megatransformer.DecoderLayer):
+        decoder_layer: megatransformer.DecoderLayer = model
+        init_weights(decoder_layer.self_attn, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        if decoder_layer.cross_attn is not None:
+            init_weights(decoder_layer.cross_attn, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        init_weights(decoder_layer.ffn, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+    elif isinstance(model, multihead_attn.MultiHeadAttention) or isinstance(model, grouped_query_attn.GroupedQueryMultiHeadAttention):
+        mha: Union[multihead_attn.MultiHeadAttention, grouped_query_attn.GroupedQueryMultiHeadAttention] = model
+        init_weights(mha.q_proj, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        init_weights(mha.k_proj, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        init_weights(mha.v_proj, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        init_weights(mha.qkv_norm, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        init_weights(mha.o_proj, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+    elif isinstance(model, infinite_multihead_attn.InfiniteMultiHeadAttention):
+        raise NotImplementedError("Weight initialization for InfiniteMultiHeadAttention not implemented.")
+    elif isinstance(model, positionwise_fcn.PositionWiseFCNetwork) or isinstance(model, phi3_mlp.Phi3MLP):
+        fcn: Union[positionwise_fcn.PositionWiseFCNetwork, phi3_mlp.Phi3MLP] = model
+        init_weights(fcn.layer_norm, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        init_weights(fcn.expand, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+        init_weights(fcn.condense, d_model, init_weights_from, init_weights_gain, tie_embeddings)
+    elif isinstance(model, millions_moe.MillionsMoE):
+        raise NotImplementedError("Weight initialization for MillionsMoE not implemented.")
+    elif isinstance(model, nn.Embedding):
+        embedding: nn.Embedding = model
+        nn.init.normal_(embedding.weight, mean=0., std=d_model ** -0.5)
+    elif isinstance(model, nn.Linear):
+        linear: nn.Linear = model
+        if init_weights_from == 'glorot_uniform':
+            nn.init.xavier_uniform_(linear.weight, gain=init_weights_gain)
+        elif init_weights_from == 'glorot_normal':
+            nn.init.xavier_normal_(linear.weight, gain=init_weights_gain)
+        elif init_weights_from == 'kaiming_uniform':
+            nn.init.kaiming_uniform_(linear.weight)
+        elif init_weights_from == 'kaiming_normal':
+            nn.init.kaiming_normal_(linear.weight)
+        elif init_weights_from == 'orthogonal':
+            nn.init.orthogonal_(linear.weight)
+        else:
+            raise Exception(f"Unknown weight initialization method: {init_weights_from}")
+        
+        if linear.bias is not None:
+            nn.init.zeros_(linear.bias)
+    elif isinstance(model, nn.LayerNorm):
+        layer_norm: nn.LayerNorm = model
+        nn.init.ones_(layer_norm.weight)
+        nn.init.zeros_(layer_norm.bias)
+    else:
+        raise Exception(f"Unknown model type: {type(model)}")
+        
+    print(f"{type(model)} initialized.")
+
+def record_model_param_stats(args, summary_writer: SummaryWriter, model, step, prefix='', embedding_tokens=None):
+    model = sanitize_model(model)
+    if isinstance(model, megatransformer.MegaTransformer):
+        record_model_param_stats(args, summary_writer, model.encoder, step, prefix='/'.join([prefix, 'encoder']), embedding_tokens=embedding_tokens)
+        record_model_param_stats(args, summary_writer, model.decoder, step, prefix='/'.join([prefix, 'decoder']), embedding_tokens=embedding_tokens)
+    elif isinstance(model, megatransformer.Encoder):
+        encoder: megatransformer.Encoder = model
+        record_model_param_stats(args, summary_writer, encoder.embed_tokens, step, prefix='/'.join([prefix, 'embed_tokens']))
+        for i, encoder_layer in enumerate(model.encoder_layers):
+            record_model_param_stats(args, summary_writer, encoder_layer, step, prefix=f'/'.join([prefix, 'encoder_layer', str(i)]))
+        record_model_param_stats(args, summary_writer, encoder.post_encoder_norm, step, prefix='/'.join([prefix, 'post_encoder_norm']))
+    elif isinstance(model, megatransformer.Decoder):
+        decoder: megatransformer.Decoder = model
+        record_model_param_stats(args, summary_writer, decoder.embed_tokens, step, prefix='/'.join([prefix, 'embed_tokens']), embedding_tokens=embedding_tokens)
+        for i, decoder_layer in enumerate(model.decoder_layers):
+            record_model_param_stats(args, summary_writer, decoder_layer, step, prefix=f'/'.join([prefix, 'decoder_layer', str(i)]))
+        record_model_param_stats(args, summary_writer, decoder.post_decoder_norm, step, prefix='/'.join([prefix, 'post_decoder_norm']))
+        record_model_param_stats(args, summary_writer, decoder.lm_head, step, prefix='/'.join([prefix, 'lm_head']))
+    elif isinstance(model, megatransformer.EncoderLayer):
+        encoder_layer: megatransformer.EncoderLayer = model
+        self_attn: nn.Module = encoder_layer.self_attn
+        ffn: nn.Module = encoder_layer.ffn
+        record_model_param_stats(args, summary_writer, self_attn, step, prefix='/'.join([prefix, 'self_attn']))
+        record_model_param_stats(args, summary_writer, ffn, step, prefix='/'.join([prefix, 'ffn']))
+    elif isinstance(model, megatransformer.DecoderLayer):
+        decoder_layer: megatransformer.DecoderLayer = model
+        self_attn: nn.Module = decoder_layer.self_attn
+        cross_attn: nn.Module = decoder_layer.cross_attn
+        ffn: nn.Module = decoder_layer.ffn
+        record_model_param_stats(args, summary_writer, self_attn, step, prefix='/'.join([prefix, 'self_attn']))
+        if cross_attn is not None:
+            record_model_param_stats(args, summary_writer, cross_attn, step, prefix='/'.join([prefix, 'cross_attn']))
+        record_model_param_stats(args, summary_writer, ffn, step, prefix='/'.join([prefix, 'ffn']))
+    elif isinstance(model, multihead_attn.MultiHeadAttention) or isinstance(model, grouped_query_attn.GroupedQueryMultiHeadAttention):
+        mha: Union[multihead_attn.MultiHeadAttention, grouped_query_attn.GroupedQueryMultiHeadAttention] = model
+        q_proj: nn.Linear = mha.q_proj
+        k_proj: nn.Linear = mha.k_proj
+        v_proj: nn.Linear = mha.v_proj
+        qkv_norm: nn.LayerNorm = mha.qkv_norm
+        out_proj: nn.Linear = mha.o_proj
+        record_model_param_stats(args, summary_writer, q_proj, step, prefix='/'.join([prefix, 'q_proj']))
+        record_model_param_stats(args, summary_writer, k_proj, step, prefix='/'.join([prefix, 'k_proj']))
+        record_model_param_stats(args, summary_writer, v_proj, step, prefix='/'.join([prefix, 'v_proj']))
+        record_model_param_stats(args, summary_writer, qkv_norm, step, prefix='/'.join([prefix, 'qkv_norm']))
+        record_model_param_stats(args, summary_writer, out_proj, step, prefix='/'.join([prefix, 'o_proj']))
+    elif isinstance(model, infinite_multihead_attn.InfiniteMultiHeadAttention):
+        raise NotImplementedError("Weight visualization for InfiniteMultiHeadAttention not implemented.")
+    elif isinstance(model, positionwise_fcn.PositionWiseFCNetwork) or isinstance(model, phi3_mlp.Phi3MLP):
+        ffn: Union[positionwise_fcn.PositionWiseFCNetwork, phi3_mlp.Phi3MLP] = model
+        record_model_param_stats(args, summary_writer, ffn.layer_norm, step, prefix='/'.join([prefix, 'layer_norm']))
+        record_model_param_stats(args, summary_writer, ffn.expand, step, prefix='/'.join([prefix, 'expand']))
+        record_model_param_stats(args, summary_writer, ffn.condense, step, prefix='/'.join([prefix, 'condense']))
+    elif isinstance(model, millions_moe.MillionsMoE):
+        raise NotImplementedError("Weight visualization for MillionsMoE not implemented.")
+    elif isinstance(model, nn.Embedding):
+        embedding: nn.Embedding = model
+        if embedding_tokens is not None:
+            metadata = embedding_tokens
+            print(f"Metadata for {prefix}: {len(metadata)}")
+        else:
+            metadata = None
+        summary_writer.add_histogram(f'{prefix}/weight', embedding.weight, step)
+        if embedding.weight.grad is not None:
+            summary_writer.add_histogram(f'{prefix}/weight_grad', embedding.weight.grad, step)
+        summary_writer.add_embedding(embedding.weight, global_step=step, tag=f'{prefix}/embedding', metadata=metadata, metadata_header=['token'])
+    elif isinstance(model, nn.Linear):
+        linear: nn.Linear = model
+        summary_writer.add_histogram(f'{prefix}/weight', linear.weight, step)
+        if linear.bias is not None:
+            summary_writer.add_histogram(f'{prefix}/bias', linear.bias, step)
+        if linear.weight.grad is not None:
+            summary_writer.add_histogram(f'{prefix}/weight_grad', linear.weight.grad, step)
+            if linear.bias is not None:
+                summary_writer.add_histogram(f'{prefix}/bias_grad', linear.bias.grad, step)
+    elif isinstance(model, nn.LayerNorm):
+        layer_norm: nn.LayerNorm = model
+        summary_writer.add_histogram(f'{prefix}/weight', layer_norm.weight, step)
+        summary_writer.add_histogram(f'{prefix}/bias', layer_norm.bias, step)
+        if layer_norm.weight.grad is not None:
+            summary_writer.add_histogram(f'{prefix}/weight_grad', layer_norm.weight.grad, step)
+            summary_writer.add_histogram(f'{prefix}/bias_grad', layer_norm.bias.grad, step)
+
+    print(f"Posted statistics for {prefix}")