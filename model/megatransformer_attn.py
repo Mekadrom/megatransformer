fixed small configuration, fixed causal mask not being large enough for sequences technically longer than max

model/megatransformer_attn.py
@@ -123,7 +123,15 @@ class MegaTransformerSelfAttention(nn.Module):
 
         # print(f"attention_scores: {attention_scores.shape}")
         
-        causal_mask_slice = self.causal_mask[:, :, :t, :T]
+        max_seq_len = max(t, T)
+        if max_seq_len > min(self.causal_mask.shape[-1], self.causal_mask.shape[-2]):
+            # recalculate causal mask with new longest length
+            self.register_buffer(
+                "causal_mask",
+                torch.tril(torch.ones(max_seq_len, max_seq_len)).view(1, 1, max_seq_len, max_seq_len),
+            )
+
+        causal_mask_slice = self.causal_mask[:, :, :t, :T].to(attention_scores.device)
         attention_scores = attention_scores.masked_fill(causal_mask_slice == 0, float("-inf"))
         
         if attention_mask is not None:

model/megatransformer_multimodal.py
@@ -4,6 +4,7 @@ from typing import Optional, Union
 
 from model import megatransformer_audio_decoder, megatransformer_blocks, megatransformer_audio_encoder, megatransformer_diffusion, megatransformer_image_encoder, megatransformer_image_decoder, megatransformer_modules
 
+import copy
 import megatransformer_utils
 import torch
 import torch.nn.functional as F
@@ -1363,7 +1364,7 @@ def create_small_multimodal_model(tokenizer: PreTrainedTokenizer, max_position_e
         max_position_embeddings=max_position_embeddings,
         n_layers=None,
         n_prelude_layers=2,
-        n_recurrent_layers=4,
+        n_recurrent_layers=2,
         n_coda_layers=2,
         intermediate_activation="swiglu",
         norm_type="rmsnorm",
@@ -1381,6 +1382,18 @@ def create_small_multimodal_model(tokenizer: PreTrainedTokenizer, max_position_e
         end_audio_token_id=end_audio_token_id,
         begin_image_token_id=begin_image_token_id,
         end_image_token_id=end_image_token_id,
+
+        audio_decoder_model_channels=64,
+        audio_decoder_time_embedding_dim=64,
+        audio_decoder_num_res_blocks=2,
+
+        audio_decoder_down_block_self_attn_n_heads=4,
+        audio_decoder_up_block_self_attn_n_heads=4,
+        audio_decoder_cross_attn_n_heads=4,
+
+        image_decoder_model_channels=64,
+        image_decoder_time_embedding_dim=64,
+        image_decoder_num_res_blocks=2,
     )
 
     config.text_prelude_config = config